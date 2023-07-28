da1fc86b3861d98272a3ba5d82719b9361053ce0
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-29 00:08:00 +0330

    Quick Start

--- Quick Start/index.js
+++ Quick Start/index.js
@@ -11,6 +11,16 @@ LEAFLET.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(MAP);
 
 //+++++++++++++++++++++++++++++++++++++++  marker
+
 let marker = LEAFLET.marker([35.6997, 51.3379]).addTo(MAP);
 
 console.log({ marker });
+
+//+++++++++++++++++++++++++++++++++++++++  circle
+
+let circle = LEAFLET.circle([35.6997, 51.3379], {
+  color: "#18edb4", // border color
+  fillColor: "#18edb4", // background color
+  fillOpacity: 0.3, // background Opacity
+  radius: 500, // شعاع
+}).addTo(MAP);
