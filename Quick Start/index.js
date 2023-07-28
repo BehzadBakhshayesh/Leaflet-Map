7b7d85f5a91a6edc39f18de95cf91e5c0ef3f195
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-28 23:57:54 +0330

    Quick Start

--- Quick Start/index.js
+++ Quick Start/index.js
@@ -1,11 +1,16 @@
 let LEAFLET = L;
-let map = LEAFLET.map("map");
+let MAP = LEAFLET.map("map");
 
-// setView([Latitude, Longitude], zoom level)
-map.setView([35.699444, 51.337776], 16);
+//+++++++++++++++++++++++++++++++++++++++ setView([Latitude, Longitude], zoom level)
+MAP.setView([35.699444, 51.337776], 16);
 
-// tile layer
+//+++++++++++++++++++++++++++++++++++++++ tile layer
 LEAFLET.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "behhhzaadddd",
-}).addTo(map);
+}).addTo(MAP);
+
+//+++++++++++++++++++++++++++++++++++++++  marker
+let marker = LEAFLET.marker([35.6997, 51.3379]).addTo(MAP);
+
+console.log({ marker });
