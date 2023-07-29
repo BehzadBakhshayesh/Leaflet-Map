d6ffcaf5f13cc03a21288684eb2f1a2cbe6c3126
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-29 22:57:06 +0330

    on Mobile

--- 3-Markers with Custom Icons/index.js
+++ 3-Markers with Custom Icons/index.js
@@ -8,34 +8,20 @@ LEAFLET.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(MAP);
 
 ////+++++++++++++++++++++++++++++++++++++++
-MAP.locate({ setView: true, maxZoom: 15 });
-////+++++++++++++++++++++++++++++++++++++++  user current locatio
+MAP.locate({ setView: true, maxZoom: 16 });
+////+++++++++++++++++++++++++++++++++++++++
 
 MAP.on("locationfound", (e) => {
-  console.log(e);
   let radius = e.accuracy;
 
   LEAFLET.marker(e.latlng)
-    .addTo(MAP)
+    .addTo(map)
     .bindPopup("You are within " + radius + " meters from this point")
     .openPopup();
 
   LEAFLET.circle(e.latlng, radius).addTo(MAP);
-  LEAFLET.circle(e.latlng, {
-    color: "#18edb4",
-    fillColor: "#18edb4",
-    fillOpacity: 0.3,
-    radius: 1000,
-  }).addTo(MAP);
 });
 
 MAP.on("locationerror", (e) => {
   alert(e.message);
 });
-
-// LEAFLET.circle(e.latlng, {
-//   color: "#18edb4", // border color
-//   fillColor: "#18edb4", // background color
-//   fillOpacity: 0.3, // background Opacity
-//   radius: 500, // شعاع
-// }).addTo(MAP);
