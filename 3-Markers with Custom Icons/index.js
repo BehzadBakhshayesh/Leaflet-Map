0f6705bf175ddfd91048579744ea2455ee21367d
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-29 23:13:42 +0330

    3-Markers with Custom Icons

--- 3-Markers with Custom Icons/index.js
+++ 3-Markers with Custom Icons/index.js
@@ -1,27 +1,19 @@
 let LEAFLET = L;
-let MAP = LEAFLET.map("map").fitWorld();
+let MAP = LEAFLET.map("map");
 
 //+++++++++++++++++++++++++++++++++++++++ tile layer
 LEAFLET.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "behhhzaadddd",
 }).addTo(MAP);
-
-////+++++++++++++++++++++++++++++++++++++++
-MAP.locate({ setView: true, maxZoom: 16 });
 ////+++++++++++++++++++++++++++++++++++++++
 
-MAP.on("locationfound", (e) => {
-  let radius = e.accuracy;
+MAP.setView([35.699444, 51.337776], 16);
 
-  LEAFLET.marker(e.latlng)
-    .addTo(map)
-    .bindPopup("You are within " + radius + " meters from this point")
-    .openPopup();
-
-  LEAFLET.circle(e.latlng, radius).addTo(MAP);
+////+++++++++++++++++++++++++++++++++++++++
+let icon = LEAFLET.icon({
+  iconUrl: "./icon/pin.png",
+  iconSize: [40, 40],
 });
 
-MAP.on("locationerror", (e) => {
-  alert(e.message);
-});
+LEAFLET.marker([35.7001, 51.3381], { icon }).addTo(MAP);
