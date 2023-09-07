a7fda333ebb22dc4c39ab6eb19e71dc9a54d19f1
bhzd <bhzd.bakhshayesh@gmail.com>
2023-09-07 13:26:53 +0330

    calculateTwoPointsDistance

--- 6-calculateTwoPointsDistance/index.js
+++ 6-calculateTwoPointsDistance/index.js
@@ -8,54 +8,6 @@ let mapOption = {
 
 let map = L.map("root", mapOption);
 
-const button = document.getElementById("button");
-
-button.addEventListener("click", () => {
-  const inputValue = document.getElementById("input").value;
-  if (navigator.geolocation) {
-    const onSuccess = (res) => {
-      const CurrentPosition = [res.coords.latitude, res.coords.longitude];
-
-      console.log(
-        Math.sqrt(
-          Math.abs(res.coords.latitude - 35.699444) ** 2 +
-            Math.abs(res.coords.longitude - 51.337776) ** 2
-        ) *
-          10 ** 5
-      );
-
-      map.setView(CurrentPosition, 16);
-
-      L.circle(CurrentPosition, {
-        color: "#18edb4",
-        fillColor: "#18edb4",
-        fillOpacity: 0.3,
-        radius: inputValue,
-      }).addTo(map);
-
-      L.circle(CurrentPosition, {
-        color: "red",
-        fillColor: "red",
-        fillOpacity: 0.9,
-        radius: 8,
-      }).addTo(map);
-
-      L.popup(CurrentPosition, {
-        content: "<p>موقعییت کنونی.</p>",
-      }).openOn(map);
-    };
-
-    const onFailed = (err) => {
-      console.log({ err });
-    };
-
-    const options = { timeout: 10000, enableHighAccuracy: true };
-
-    navigator.geolocation.getCurrentPosition(onSuccess, onFailed, options);
-  } else {
-    console.log("err message");
-  }
-});
 let a, b;
 map.on("click", (e) => {
   if (!a) {
